b2c25ebd15d57be6bd9a2a99dda88c66fae46ec5
kiss9815 <kiss9815@gmail.com>
2016-01-08 17:52:10 +0900

    1월 8일 금요일 수업 종료

--- app/src/main/java/com/example/tacademy/helloandroid/MainActivity.java
+++ app/src/main/java/com/example/tacademy/helloandroid/MainActivity.java
@@ -2,12 +2,31 @@ package com.example.tacademy.helloandroid;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final TextView textView = (TextView)findViewById(R.id.text_message);
+        Button btn_send = (Button)findViewById(R.id.btn_send);
+        final EditText editText = (EditText)findViewById(R.id.edit_text);
+
+        btn_send.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                textView.setText(editText.getText().toString());
+            }
+        });
+
     }
 }
